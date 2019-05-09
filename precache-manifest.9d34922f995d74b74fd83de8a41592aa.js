b4783ce5793d73302c61e6dadb62d9c88a5426d4
daliabdmalek <49783051+daliabdmalek@users.noreply.github.com>
2019-05-09 11:20:00 +0200

    Updates

---
+++ precache-manifest.9d34922f995d74b74fd83de8a41592aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "094d9f969e6063e244062ab1fc14ca98",
+    "url": "/Static-Timer-/index.html"
+  },
+  {
+    "revision": "ed46ae9b477464f4658d",
+    "url": "/Static-Timer-/static/css/main.793a0b6c.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/Static-Timer-/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "ed46ae9b477464f4658d",
+    "url": "/Static-Timer-/static/js/main.74b4b22e.chunk.js"
+  },
+  {
+    "revision": "1adf4af8f6f77a0bcbc0",
+    "url": "/Static-Timer-/static/js/runtime~main.0b29ab45.js"
+  },
+  {
+    "revision": "9365e93e1a37b34e9b6b8c6c027b2485",
+    "url": "/Static-Timer-/static/media/beach-caribbean-jetty-237272.9365e93e.jpg"
+  }
+]);
